59b3e6b3a2e44249dfc49c778635318b7d626e18
Aleksei Semochkin <aleksioprime@gmail.com>
2026-08-03 13:22:05 +0300

    Добавлено значение FORWARDED_ALLOW_IPS в .env.example и обновлен entrypoint.sh для поддержки forwarded-заголовков

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,5 +2,8 @@
 set -e
 
 alembic upgrade head
-exec uvicorn src.main:app --host 0.0.0.0 --port 8000
-
+exec uvicorn src.main:app \
+  --host 0.0.0.0 \
+  --port 8000 \
+  --proxy-headers \
+  --forwarded-allow-ips "${FORWARDED_ALLOW_IPS:-*}"
